9e31c5a6218d0e6efc80ae56e119699ed404b0de
spectre <spectrxtt@gmail.com>
2024-08-27 14:54:30 -0600

    cam

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginComponent from './components/loginComponent';
 import Home from './components/home';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
 function App() {
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginComponent />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LoginComponent />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
